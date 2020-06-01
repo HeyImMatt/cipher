170d741ffc2657763b114259033c0389e9ab4ebe
heyimmatt <mrmcfarland@gmail.com>
2020-06-01 15:52:35 -0700

    Log to the console the user's input sentence and then the final ciphered sentence
    Co-authored-by: David Cole <davidtcole@yahoo.com>
    Co-authoered-by: Allison Sadin <aesadin@gmail.com>

--- js/cipher.js
+++ js/cipher.js
@@ -1,25 +1,32 @@
+let userSentence;
+let cipherSentence;
+
 function getUserInput() {
-  const userSentence = prompt('Please enter a sentence to encode:');
-  console.log(userSentence);
+  userSentence = prompt('Please enter a sentence to encode:');
   capitalizeFirstAndLastLetter(userSentence);
 }
 
 function capitalizeFirstAndLastLetter(userSentence) {
-  let firstAndLastLetter = userSentence.substring(0, 1) + userSentence.slice(-1);
-  console.log(firstAndLastLetter.toUpperCase())
+  const firstAndLastLetter = userSentence.substring(0, 1) + userSentence.slice(-1);
   reverseLettersAndAdd(firstAndLastLetter.toUpperCase(), userSentence);
 }
 
 function reverseLettersAndAdd(letters, userSentence) {
   const combinedSentence = userSentence + (letters.split("").reverse().join(""));
-  console.log(combinedSentence)
   insertLetter(userSentence, combinedSentence)
 }
 
 function insertLetter(userSentence, combinedSentence) {
-  const letterToRemove = parseInt(userSentence.length / 2)
+  const letterToRemove = Math.floor((userSentence.length / 2))
   const result = userSentence.slice(letterToRemove, letterToRemove + 1) + combinedSentence;
-  console.log(result)
+  reverseCipherSentence(result);
+}
+
+function reverseCipherSentence(result) {
+  cipherSentence = (result.split("").reverse().join(""));
 }
 
 getUserInput();
+
+console.log(userSentence);
+console.log(cipherSentence);
